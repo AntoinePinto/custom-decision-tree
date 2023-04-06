34009bd15d8e4c5e24063ad9bc48c7797fd6c310
Antoine Pinto <antoine.pinto1@outlook.fr>
2023-04-06 19:17:29 +0200

    Add docstring

--- custom_tree.py
+++ custom_tree.py
@@ -4,8 +4,16 @@ import numpy as np
 
 from tqdm import tqdm
 
-
 def get_metric(metric_vars):
+    """
+    Computes a performance metric based on the input data.
+
+    Args:
+    - metric_vars (np.ndarray): an array containing the input data to be used to compute the metric.
+
+    Returns:
+    - metric (float): the computed performance metric.
+    """
 
     y = metric_vars[:, 0]
     odds = metric_vars[:, 1]
@@ -15,6 +23,17 @@ def get_metric(metric_vars):
     return metric
 
 def get_delta(split, metric_vars, optimisation_method='minimisation'):
+    """
+    Computes the performance gain from splitting the input data into two subsets based on the specified split.
+
+    Args:
+    - split (np.ndarray): a boolean array indicating which samples belong to the first subset.
+    - metric_vars (np.ndarray): an array containing the input data to be used to compute the metric.
+    - optimisation_method (str): a string indicating whether to optimize for maximization or minimization.
+
+    Returns:
+    - delta (float): the computed performance gain.
+    """
 
     delta =  get_metric(metric_vars) - (get_metric(metric_vars[split]) + get_metric(metric_vars[np.invert(split)]))
 
@@ -24,6 +43,18 @@ def get_delta(split, metric_vars, optimisation_method='minimisation'):
     return delta
 
 def get_best_split_from_var(var, metric_vars, optimisation_method='minimisation'):
+    """
+    Finds the best split of the input data based on the values of a specific variable.
+
+    Args:
+    - var (np.ndarray): an array containing the values of the variable to be used to split the data.
+    - metric_vars (np.ndarray): an array containing the input data to be used to compute the metric.
+    - optimisation_method (str): a string indicating whether to optimize for maximization or minimization.
+
+    Returns:
+    - best_split (float): the value of the variable that yields the best split.
+    - optimum_value (float): the performance gain obtained by splitting the data at the best split value.
+    """
 
     splits = np.sort(np.unique(var))[:-1]
 
@@ -41,12 +72,31 @@ def get_best_split_from_var(var, metric_vars, optimisation_method='minimisation'
     return best_split, optimum_value
 
 class CustomDecisionTreeClassifier:
+    """
+    A custom decision tree classifier.
+
+    Attributes:
+    - max_depth (int): the maximum depth of the decision tree.
+    - partitions (dict): a dictionary containing the partitions of the decision tree.
+    - splitting (dict): a dictionary containing the splitting rules of the decision tree.
+    """
 
     def __init__(self, max_depth=5):
 
         self.max_depth = max_depth
 
     def fit(self, X, y, metric_vars, optimisation_method='minimisation'):
+        """Fit the decision tree to the training data.
+
+        Args:
+        - X (array-like): the training input samples.
+        - y (array-like): the target values.
+        - metric_vars (array-like): the variables used to calculate the metric.
+        - optimisation_method (str, optional): the optimization method used to find the best split (default: 'minimisation').
+
+        Returns:
+        None
+        """
 
         metric_vars = np.array(metric_vars)
         y = np.array(y)
@@ -103,7 +153,17 @@ class CustomDecisionTreeClassifier:
                 self.splitting = splitting
 
     def predict_proba_x(self, x, return_exp_metric=False):
+        """Predict the class probabilities for a single input sample.
 
+        Args:
+        - x (array-like): a single input sample.
+        - return_exp_metric (bool, optional): whether to return the expected metric for the leaf node (default: False).
+
+        Returns:
+        - probas (list): the predicted class probabilities.
+        - expected_metric (float, optional): the expected metric for the leaf node (only if `return_exp_metric=True`).
+        """
+        
         id_partition = 1
         while True:
 
@@ -125,15 +185,33 @@ class CustomDecisionTreeClassifier:
                 id_partition = id_partition * 2 + 1
 
     def predict_proba(self, X, return_exp_metric=False):
+        """Predict the class probabilities for multiple input samples.
 
+        Args:
+        - X (array-like): the input samples.
+        - return_exp_metric (bool, optional): whether to return the expected metric for the leaf nodes (default: False).
+
+        Returns:
+        - probas (list): a list of the predicted class probabilities for each input sample.
+        - expected_metric (float, optional): a list of the expected metrics for each leaf node (only if `return_exp_metric=True`).
+        """
         X = np.array(X)
 
         probas = list(map(partial(self.predict_proba_x, return_exp_metric=return_exp_metric), [X[i,:] for i in range(len(X))]))
 
         return probas
 
-    def print_tree(self, max_depth=1000, features_names=None, show_delta=True, show_metric=True, show_repartition=True):
+    def print_tree(self, max_depth=1000, features_names=None, show_delta=True, show_metric=True, show_repartition=True, digits=100):
+        """Print the decision tree.
 
+        Args:
+        - max_depth (int, optional): the maximum depth to print (default: 1000).
+        - features_names (list, optional): a list of the feature names (default: None).
+        - show_delta (bool, optional): whether to show the delta value for each split (default: True).
+        - show_metric (bool, optional): whether to show the metric value for each leaf node (default: True).
+        - show_repartition (bool, optional): whether to show the class repartition for each leaf node (default: True).
+        - digits (int, optional): the number of digits to round the split values (default: 100).
+        """
         id_partition = 1
         while True:
 
@@ -153,14 +231,14 @@ class CustomDecisionTreeClassifier:
                     var = features_names[var] if features_names is not None else f'feature {var}'
                     split_value = self.splitting[id_first_parent]['split_value']
                     split_type = '>' if id_partition % 2 == 1 else '<='
-                    print(f" | {var} {split_type} {split_value}", end = '')
+                    print(f" | {var} {split_type} {round(split_value, digits)}", end = '')
                     if show_delta is True:
                         delta = self.splitting[id_first_parent]['delta']
-                        print(f" | Δ = {delta}", end = '')
+                        print(f" | Δ = {round(delta, digits)}", end = '')
 
                 if show_metric is True:
                     metric = part['metric']
-                    print(f" -> metric = {metric}", end="")
+                    print(f" -> metric = {round(metric, digits)}", end="")
                 if show_repartition is True:
                     print(f" | repartition = {part['repartition']}", end="")
                 print('')
@@ -184,7 +262,14 @@ class CustomRandomForestClassifier:
         self.max_depth = max_depth
 
     def fit(self, X, y, metric_vars, optimisation_method='minimisation'):
+        """Fit the random forest to the training data.
 
+        Args:
+        - X (array-like): the training input samples.
+        - y (array-like): the target values.
+        - metric_vars (array-like): the variables used to calculate the metric.
+        - optimisation_method (str, optional): the optimization method used to find the best split (default: 'minimisation').
+        """
         metric_vars = np.array(metric_vars)
         y = np.array(y)
         X = np.array(X)
@@ -198,7 +283,6 @@ class CustomRandomForestClassifier:
             model = CustomDecisionTreeClassifier(max_depth=self.max_depth)
             model.fit(X=X[sub_obs,:][:,sub_var], y=y[sub_obs], metric_vars=metric_vars[sub_obs], optimisation_method=optimisation_method)
 
-            # Correct variable id that are incorectly encoded in model object due to variable sampling
             [d.update({'id_var': sub_var[d['id_var']]}) for d in model.splitting.values()]
 
             forest[id_estimator] = {'sub_obs': sub_obs, 'model': model}
@@ -206,7 +290,16 @@ class CustomRandomForestClassifier:
         self.forest = forest
 
     def predict_proba_x(self, x, return_exp_metric=False):
+        """Predict the class probabilities for a single input sample.
 
+        Args:
+        - x (array-like): a single input sample.
+        - return_exp_metric (bool, optional): whether to return the expected metric for the leaf node (default: False).
+
+        Returns:
+        - probas (list): the predicted class probabilities.
+        - expected_metric (float, optional): the expected metric for the leaf node (only if `return_exp_metric=True`).
+        """
         estimators_probas = []
         for id_estimator in self.forest:
             m = self.forest[id_estimator]['model']
@@ -222,7 +315,16 @@ class CustomRandomForestClassifier:
             return probas
 
     def predict_proba(self, X, return_exp_metric=False):
+        """Predict the class probabilities for multiple input samples.
 
+        Args:
+        - X (array-like): the input samples.
+        - return_exp_metric (bool, optional): whether to return the expected metric for the leaf nodes (default: False).
+
+        Returns:
+        - probas (list): a list of the predicted class probabilities for each input sample.
+        - expected_metric (float, optional): a list of the expected metrics for each leaf node (only if `return_exp_metric=True`).
+        """
         X = np.array(X)
 
         probas = list(map(partial(self.predict_proba_x, return_exp_metric=return_exp_metric), [X[i,:] for i in range(len(X))]))
